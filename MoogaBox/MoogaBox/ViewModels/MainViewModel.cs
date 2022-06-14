90dd2d9afd0e4c6c26f45e90dabfe2fd99f89890
ynns1217 <ynns1217@naver.com>
2022-06-14 17:38:16 +0900

    220614 시작

---
+++ MoogaBox/MoogaBox/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using DocumentFormat.OpenXml.Bibliography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoogaBox.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        private string id;
+        private string pw;
+
+        public string Id
+        {
+            get => id; set
+            {
+                id = value;
+            }
+        }   
+        public string Pw
+        {
+            get => pw; set
+            {
+                pw = value;
+            }
+        }
+        //InsertPW
+    }
+}
---
+++ MoogaBox/MoogaBox/Views/MainView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+namespace MoogaBox.Views
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainView : MetroWindow
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
